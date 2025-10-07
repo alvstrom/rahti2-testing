a7beef6ec6d0fd3964a59d589cc834591556de33
Alvin Ström <127141139+alvstrom@users.noreply.github.com>
2025-10-07 11:00:54 +0300

    Refactor server.js for improved readability and update response message

--- src/server.js
+++ src/server.js
@@ -1,22 +1,21 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const PORT = process.env.PORT || 8080
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const FOO = process.env.FOO || "foo not found";
 
-console.log(`Node.js ${process.version}`)
+console.log(`Node.js ${process.version}`);
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({ msg: "Rahti2 node 0.2" })
-})
-
+app.get("/", (req, res) => {
+  res.json({ msg: "Rahti2 node 0.3" });
+});
 
 app.listen(PORT, () => {
-    try {
-        console.log(`Running on http://localhost:${PORT}`)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-    
-})
+  try {
+    console.log(`Running on http://localhost:${PORT}`);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
